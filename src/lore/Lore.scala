9223133755c3a187800cf9402c86083f4aca579b
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-03-28 01:53:06 +0200

    Move calculation.lore to example folder.

--- src/lore/Lore.scala
+++ src/lore/Lore.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 
 object Lore {
   def main(args: Array[String]): Unit = {
-    val source = Source.fromFile("calculation.lore").getLines.mkString
+    val source = Source.fromFile("examples/calculation.lore").getLines.mkString
     val parser = new FpExpressionParser()
     val expression = parser.parseExpression(source)
     val result = ExprAlgebra.evaluate(ExprAlgebra.evalAlgebra)(expression)
