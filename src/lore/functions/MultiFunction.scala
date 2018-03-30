eada1e77c9944691a424d85372b79834d36ac5d1
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-03-30 04:03:22 +0200

    Quick fix because of the singleton tuple requirement. We have to think about this in the future.

--- src/lore/functions/MultiFunction.scala
+++ src/lore/functions/MultiFunction.scala
@@ -5,6 +5,7 @@ import lore.types.Type
 case class MultiFunction(name: String, functions: Set[LoreFunction]) {
 
   def fit(tpe: Type): Set[LoreFunction] = {
+    //functions.foreach(f => println(s"$tpe <= ${f.inputType}? ${f.inputType.isSupertype(tpe)}"))
     functions.filter(_.inputType.isSupertype(tpe))
   }
 
