7450d35148fc72caf553ba6ac1dc3589a1f68d24
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-04-02 21:07:35 +0200

    Ensure that external input types are always tuples.

--- src/lore/functions/MultiFunction.scala
+++ src/lore/functions/MultiFunction.scala
@@ -6,14 +6,14 @@ case class MultiFunction(name: String, functions: Set[LoreFunction]) {
 
   def fit(t: Type): Set[LoreFunction] = {
     //functions.foreach(f => println(s"t <= ${f.inputType}? ${Subtyping.isSubtype(t, f.inputType)}"))
-    functions.filter(f => Subtyping.isSubtype(t, f.inputType))
+    functions.filter(f => Subtyping.isSubtype(t.toTuple, f.inputType))
   }
 
   /**
     * @return The function with the exact given input type.
     */
   def function(inputType: Type): Option[LoreFunction] = {
-    functions.find(f => f.inputType == inputType)
+    functions.find(f => f.inputType == inputType.toTuple)
   }
 
 }
