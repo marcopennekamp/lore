b341aff25218c9eee365cb84464607f49c848706
marcopennekamp <marco.pennekamp96@gmail.com>
2019-12-25 03:29:39 +0100

    Actually print the left type.

--- src/lore/functions/MultiFunction.scala
+++ src/lore/functions/MultiFunction.scala
@@ -5,7 +5,7 @@ import lore.types.{Subtyping, Type}
 case class MultiFunction(name: String, functions: Set[LoreFunction]) {
 
   def fit(t: Type): Set[LoreFunction] = {
-    //functions.foreach(f => println(s"t <= ${f.inputType}? ${Subtyping.isSubtype(t, f.inputType)}"))
+    //functions.foreach(f => println(s"$t <= ${f.inputType}? ${Subtyping.isSubtype(t, f.inputType)}"))
     functions.filter(f => Subtyping.isSubtype(t.toTuple, f.inputType))
   }
 
