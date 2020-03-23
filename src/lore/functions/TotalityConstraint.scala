d65c42c74c9056d70d3356ef30855826de4a3d16
marcopennekamp <marco.pennekamp96@gmail.com>
2020-03-23 13:16:55 +0100

    Simplify the totality constraint according to the new spec.

--- src/lore/functions/TotalityConstraint.scala
+++ src/lore/functions/TotalityConstraint.scala
@@ -16,15 +16,15 @@ object TotalityConstraint {
    */
 
   /**
-    * @return The set of functions that are not fully implemented. If the set is empty, the multi-function satisfies
-    *         the totality constraint.
+    * Verifies the multi-function for the totality constraint. In case of a violation, returns the set of functions
+    * that are not fully implemented. If this set is empty, the multi-function satisfies the totality constraint.
     */
   def verify(mf: MultiFunction)(implicit context: Context): Set[LoreFunction] = {
     mf.functions.filter(_.isAbstract).filterNot(isVerified(mf))
   }
 
   /**
-    * @return Whether the given abstract function satisfies the totality constraint.
+    * Verifies whether the given abstract function satisfies the totality constraint.
     */
   private def isVerified(mf: MultiFunction)(f: LoreFunction)(implicit context: Context): Boolean = {
     // We need to use the direct declared subtypes of the ABSTRACT parameters. An example will clear this up:
@@ -34,11 +34,7 @@ object TotalityConstraint {
     //    directDeclaredSubtypes, because it would substitute B1 and B2 for B, leaving B out of the equation entirely.
     f.inputType.abstractDirectDeclaredSubtypes.forall { subtype =>
       mf.functions.exists { f2 =>
-        Subtyping.isStrictSubtype(f2.inputType, f.inputType) &&
-          // TODO: The isVerified might be unnecessary since it gets checked anyway. This means we are essentially
-          //       verifying f2 twice or more times.
-          (!f2.isAbstract || (f2.isAbstract && isVerified(mf)(f2))) &&
-          mf.fit(subtype).contains(f2)
+        Subtyping.isStrictSubtype(f2.inputType, f.inputType) && mf.fit(subtype).contains(f2)
       }
     }
   }
