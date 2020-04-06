292e17a01e308929939c9474a9c5497599b991d7
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-06 23:20:01 +0200

    Consider supertype properties/members in entity/class definitions.

--- src/lore/definitions/ClassDefinition.scala
+++ src/lore/definitions/ClassDefinition.scala
@@ -2,6 +2,11 @@ package lore.definitions
 
 import lore.types.ClassType
 
-class ClassDefinition(override val name: String, override val tpe: ClassType, override val properties: List[PropertyDefinition]) extends DataTypeDefinition {
+/**
+  * @param localProperties The properties declared within this class. Does not include supertype properties.
+  */
+class ClassDefinition(override val name: String, override val tpe: ClassType, val localProperties: List[PropertyDefinition]) extends DataTypeDefinition {
   override def supertypeDefinition: Option[ClassDefinition] = tpe.supertype.map(_.definition)
+  override def properties: List[PropertyDefinition] = supertypeDefinition.map(_.properties).getOrElse(List.empty) ++ localProperties
+  override def members: List[MemberDefinition] = properties
 }
--- src/lore/definitions/EntityDefinition.scala
+++ src/lore/definitions/EntityDefinition.scala
@@ -2,8 +2,16 @@ package lore.definitions
 
 import lore.types.EntityType
 
-class EntityDefinition(override val name: String, override val tpe: EntityType, val members: List[MemberDefinition]) extends DataTypeDefinition {
+/**
+  * @param localMembers The members declared within this entity. Does not include supertype members.
+  */
+class EntityDefinition(
+  override val name: String,
+  override val tpe: EntityType,
+  val localMembers: List[MemberDefinition]
+) extends DataTypeDefinition {
   override def supertypeDefinition: Option[DataTypeDefinition] = tpe.supertype.map(_.definition)
+  override lazy val members: List[MemberDefinition] = supertypeDefinition.map(_.members).getOrElse(List.empty) ++ localMembers
   override def properties: List[PropertyDefinition] = members.flatMap { case property: PropertyDefinition => Some(property); case _ => None }
   def components: List[ComponentDefinition] = members.flatMap { case component: ComponentDefinition => Some(component); case _ => None }
 }
