38415fab48473a329a1898d1a2bbbcbe781fb2e2
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-07 15:08:10 +0200

    Add TODO. Reorder DeclNode.

--- src/lore/ast/DeclNode.scala
+++ src/lore/ast/DeclNode.scala
@@ -13,32 +13,32 @@ object DeclNode {
     outputType: TypeExprNode, body: Expr,
   ) extends DeclNode
   case class ParameterNode(name: String, tpe: TypeExprNode)
+}
 
-  /**
-    * Top-level type declarations.
-    */
-  sealed trait TypeDeclNode extends DeclNode
-  object TypeDeclNode {
-    // TODO: We need to add type aliases to the spec... or remove them from the implementation.
-    case class AliasNode(name: String, tpe: TypeExprNode) extends TypeDeclNode
+/**
+  * Top-level type declarations.
+  */
+sealed trait TypeDeclNode extends DeclNode
+object TypeDeclNode {
+  // TODO: We need to add type aliases to the spec... or remove them from the implementation.
+  case class AliasNode(name: String, tpe: TypeExprNode) extends TypeDeclNode
 
-    case class LabelNode(name: String, supertypeName: Option[String]) extends TypeDeclNode
+  case class LabelNode(name: String, supertypeName: Option[String]) extends TypeDeclNode
 
-    case class ClassNode(
-      name: String, supertypeName: Option[String], isAbstract: Boolean,
-      properties: List[PropertyNode], constructors: List[ConstructorNode],
-    ) extends TypeDeclNode
+  case class ClassNode(
+    name: String, supertypeName: Option[String], isAbstract: Boolean,
+    properties: List[PropertyNode], constructors: List[ConstructorNode],
+  ) extends TypeDeclNode
 
-    case class EntityNode(
-      name: String, supertypeName: Option[String], isAbstract: Boolean,
-      members: List[MemberNode], constructors: List[ConstructorNode],
-    ) extends TypeDeclNode
+  case class EntityNode(
+    name: String, supertypeName: Option[String], isAbstract: Boolean,
+    members: List[MemberNode], constructors: List[ConstructorNode],
+  ) extends TypeDeclNode
 
-    sealed trait MemberNode
-    case class PropertyNode(name: String, tpe: TypeExprNode, isMutable: Boolean) extends MemberNode
-    case class ComponentNode(name: String, nominalType: TypeExprNode.NominalNode) extends MemberNode
+  sealed trait MemberNode
+  case class PropertyNode(name: String, tpe: TypeExprNode, isMutable: Boolean) extends MemberNode
+  case class ComponentNode(name: String, nominalType: TypeExprNode.NominalNode) extends MemberNode
 
-    // TODO: Implement constructor AST.
-    case class ConstructorNode()
-  }
+  // TODO: Implement constructor AST.
+  case class ConstructorNode()
 }
--- src/lore/compiler/CompilationErrors.scala
+++ src/lore/compiler/CompilationErrors.scala
@@ -1,5 +1,8 @@
 package lore.compiler
 
+// TODO: Find a better way to accumulate errors (maybe with a monad) and report all errors at once, instead of
+//       terminating after finding only one error. This applies to type checks, constraint checks, illegal expressions,
+//       illegal declarations, and so on.
 object CompilationErrors {
   case class FunctionNotFound(name: String) extends RuntimeException(s"The function $name does not exist in the current scope.")
   case class TypeNotFound(name: String) extends RuntimeException(s"The type $name does not exist in the current scope.")
