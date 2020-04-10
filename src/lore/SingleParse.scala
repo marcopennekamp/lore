572f87cacb7c8b7a4fe96257ede8f929434a91b7
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-11 00:50:12 +0200

    Add SingleParse, which can be used to run a single parse.

---
+++ src/lore/SingleParse.scala
@@ -0,0 +1,24 @@
+package lore
+
+import fastparse._
+import lore.parser.StatementParser
+
+import ScalaWhitespace._
+
+/**
+  * This object can be run (for example from within the IDE) to parse a single statement (or other code if the parser
+  * is changed) for debugging and logging purposes. Running tests with fastparse logging is messy as it provides way
+  * too much useless data from tests that aren't relevant to the problem, but logged anyway. This object can be used,
+  * in conjunction with P(...).log, to pinpoint issues with a parsing run.
+  */
+object SingleParse {
+  private val source = "0.0"
+
+  def main(args: Array[String]): Unit = {
+    def file[_: P] = P(StatementParser.statement ~ End)
+    fastparse.parse(source, file(_)) match {
+      case Parsed.Success(result, _) => println(result)
+      case Parsed.Failure(_, _, extra) => println(extra.trace().aggregateMsg)
+    }
+  }
+}
