2f80244a394159f98f686a249d7a7c0a63824894
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-10 17:54:03 +0200

    Use PrecedenceParser in TypeParser.

--- src/lore/parser/TypeParser.scala
+++ src/lore/parser/TypeParser.scala
@@ -7,15 +7,18 @@ import ScalaWhitespace._
 object TypeParser {
   import LexicalParser.identifier
 
-  // The parser hierarchy implements "operator precedence". Hence parsers also may be ordered differently
-  // than their nodes are declared in the AST files.
-  def typeExpression[_: P]: P[TypeExprNode] = P(sumType)
-
-  private def sumType[_: P]: P[TypeExprNode] = P(xarySet("|", intersectionType, TypeExprNode.SumNode) | intersectionType)
-
-  private def intersectionType[_: P]: P[TypeExprNode] = P(xarySet("&", mapType, TypeExprNode.IntersectionNode) | mapType)
-
-  private def mapType[_: P]: P[TypeExprNode] = P(binary("->", atom, TypeExprNode.MapNode) | atom)
+  def typeExpression[_: P]: P[TypeExprNode] = {
+    import PrecedenceParser._
+    PrecedenceParser.parser(
+      operator = StringIn("|", "&", "->"),
+      operand = atom,
+      operatorMeta = Map(
+        "|" -> XaryOperator(1, (list: List[TypeExprNode]) => TypeExprNode.SumNode(list.toSet)),
+        "&" -> XaryOperator(2, (list: List[TypeExprNode]) => TypeExprNode.IntersectionNode(list.toSet)),
+        "->" -> BinaryOperator(3, TypeExprNode.MapNode),
+      ),
+    )
+  }
 
   private def atom[_: P]: P[TypeExprNode] = P(unitType | productType | listType | componentType | nominalType | enclosedType)
 
