630e33d5bc089b05e44c8b2cd840ed499694eb97
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-11 00:11:10 +0200

    Reorganize for code quality in StatementParserSpec.

--- test/lore/test/parser/StatementParserSpec.scala
+++ test/lore/test/parser/StatementParserSpec.scala
@@ -12,18 +12,25 @@ class StatementParserSpec extends BaseSpec with ParserSpecExtensions[StmtNode] {
 
   override def parser[_: P] = StatementParser.statement
 
-  "Strings, escapes, and interpolations" should "be parsed correctly and economically" in {
+  private val va = VariableNode("a")
+  private val vb = VariableNode("b")
+  private val vc = VariableNode("c")
+  private val vi = VariableNode("i")
+  private val vk = VariableNode("k")
+  private val vx = VariableNode("x")
+
+  "The statement parser" should "parse strings, escapes, and interpolations correctly" in {
     "''" --> StringLiteralNode("")
     "'   '" --> StringLiteralNode("   ")
     "'\\n'" --> StringLiteralNode("\n")
     "'\\n\\t\\r\\'\\$\\\\'" --> StringLiteralNode("\n\t\r'$\\")
     "'test $x\\n\\t\\u0394'" --> ConcatenationNode(List(
       StringLiteralNode("test "),
-      VariableNode("x"),
+      vx,
       StringLiteralNode("\n\t\u0394")
     ))
     "'$myLongVariable'" --> VariableNode("myLongVariable")
-    "'${x}'" --> VariableNode("x")
+    "'${x}'" --> vx
     "'\\${quite}$some\\$confusion in ${this} town'" --> ConcatenationNode(List(
       StringLiteralNode("${quite}"),
       VariableNode("some"),
@@ -35,10 +42,10 @@ class StatementParserSpec extends BaseSpec with ParserSpecExtensions[StmtNode] {
     apples --> ConcatenationNode(List(
       PropertyAccessNode(VariableNode("p"), List("name")),
       StringLiteralNode(", you have "),
-      VariableNode("k"),
+      vk,
       StringLiteralNode(" apples. Please claim your "),
       IfElseNode(
-        LessThanNode(VariableNode("k"), IntLiteralNode(10)),
+        LessThanNode(vk, IntLiteralNode(10)),
         StringLiteralNode("free"),
         StringLiteralNode("1000$"),
       ),
@@ -46,89 +53,87 @@ class StatementParserSpec extends BaseSpec with ParserSpecExtensions[StmtNode] {
     ))
   }
 
-  "Statements and top-level expressions" should "only appear in blocks, conditionals, and repetitions" in {
-
+  it should "parse operators correctly" in {
+    "a + b" --> AdditionNode(va, vb)
+    "a - b" --> SubtractionNode(va, vb)
+    "a * b + b * a" --> AdditionNode(MultiplicationNode(va, vb), MultiplicationNode(vb, va))
+    "a / b" --> DivisionNode(va, vb)
+    "a < b" --> LessThanNode(va, vb)
   }
 
-  "Operators" should "be parsed correctly" in {
-    val a = VariableNode("a")
-    val b = VariableNode("b")
-    val c = VariableNode("c")
-    "a + b" --> AdditionNode(a, b)
-    "a + { b }" --> AdditionNode(a, BlockNode(List(b)))
-    "a < b" --> LessThanNode(a, b)
-    "a + -b" --> AdditionNode(a, NegationNode(b))
+  it should "parse complex operator expressions correctly" in {
+    "a + { b }" --> AdditionNode(va, BlockNode(List(vb)))
+    "a + -b" --> AdditionNode(va, NegationNode(vb))
     "~(a == b) | (a < ~b) & (~a < c)" --> DisjunctionNode(List(
-      LogicalNotNode(EqualsNode(a, b)),
+      LogicalNotNode(EqualsNode(va, vb)),
       ConjunctionNode(List(
-        LessThanNode(a, LogicalNotNode(b)),
-        LessThanNode(LogicalNotNode(a), c),
+        LessThanNode(va, LogicalNotNode(vb)),
+        LessThanNode(LogicalNotNode(va), vc),
       )),
     ))
     "~a & ~b & c + -a * -b" --> ConjunctionNode(List(
-      LogicalNotNode(a),
-      LogicalNotNode(b),
+      LogicalNotNode(va),
+      LogicalNotNode(vb),
       AdditionNode(
-        c,
+        vc,
         MultiplicationNode(
-          NegationNode(a),
-          NegationNode(b),
+          NegationNode(va),
+          NegationNode(vb),
         ),
       ),
     ))
   }
 
-  "Comparisons" should "be parsed correctly" in {
-
-  }
-
-  "Conditionals and repetitions" should "be parsed correctly" in {
+  it should "parse conditionals and repetitions correctly" in {
     "if (true) false" --> IfElseNode(BoolLiteralNode(true), BoolLiteralNode(false), UnitNode)
     "if (i < 25) { i = i + 1 }" --> IfElseNode(
-      LessThanNode(VariableNode("i"), IntLiteralNode(25)),
+      LessThanNode(vi, IntLiteralNode(25)),
       BlockNode(List(
         AssignmentNode(
           AddressNode(List("i")),
-          AdditionNode(VariableNode("i"), IntLiteralNode(1)),
+          AdditionNode(vi, IntLiteralNode(1)),
         ),
       )),
       UnitNode
     )
   }
 
-  "A block-rich expression" should "be parsed within 50 milliseconds" in {
+  it should "parse a block-rich expression within 50 milliseconds" in {
     timed(50) { () =>
       "{ a + { b } + { b }.x + b }" --> BlockNode(List(
-        AdditionNode(AdditionNode(AdditionNode(
-          VariableNode("a"),
-          BlockNode(List(VariableNode("b")))),
-          PropertyAccessNode(
-            BlockNode(List(VariableNode("b"))),
-            List("x"))
+        AdditionNode(
+          AdditionNode(
+            AdditionNode(va, BlockNode(List(vb))),
+            PropertyAccessNode(BlockNode(List(vb)), List("x")),
+          ),
+          vb,
         ),
-          VariableNode("b")),
       ))
     }
   }
 
-  "A block-rich expression repeated 10,000 times" should "should be parsed within 5 seconds" in {
+  it should "parse a block-rich expression repeated 10,000 times within 5 seconds" in {
     timed(5000) { () =>
       val repetitions = 10000
       "{" + "a + { if (a < 10) a + 10 else b + 10 } + b\n".repeat(repetitions) + "}" --> BlockNode(
         List.fill(repetitions)(
           AdditionNode(
             AdditionNode(
-              VariableNode("a"),
+              va,
               BlockNode(List(IfElseNode(
-                LessThanNode(VariableNode("a"), IntLiteralNode(10)),
-                AdditionNode(VariableNode("a"), IntLiteralNode(10)),
-                AdditionNode(VariableNode("b"), IntLiteralNode(10)),
+                LessThanNode(va, IntLiteralNode(10)),
+                AdditionNode(va, IntLiteralNode(10)),
+                AdditionNode(vb, IntLiteralNode(10)),
               )))
             ),
-            VariableNode("b"),
+            vb,
           ),
         ),
       )
     }
+  }
+
+  "Statements and top-level expressions" should "only appear in blocks, conditionals, and repetitions" in {
+
   }
 }
