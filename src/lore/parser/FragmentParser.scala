588ae067a61a6f7c6db35f913bf511fb0600f8d2
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-12 01:48:45 +0200

    Add entity parser to topDeclaration.
    Make parsers private wherever possible.

--- src/lore/parser/FragmentParser.scala
+++ src/lore/parser/FragmentParser.scala
@@ -29,22 +29,22 @@ object FragmentParser {
     P(Space.WL ~~ topDeclaration.repX(0, Space.terminators) ~~ Space.WL ~~ End)
   }
 
-  def topDeclaration[_: P]: P[DeclNode] = P(function | action | typeDeclaration)
+  private def topDeclaration[_: P]: P[DeclNode] = P(function | action | typeDeclaration)
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Function declarations.
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  def function[_: P]: P[DeclNode.FunctionNode] = {
+  private def function[_: P]: P[DeclNode.FunctionNode] = {
     P("function" ~/ identifier ~ parameters ~ ":" ~ typeExpression ~ ("=" ~ expression).?).map(DeclNode.FunctionNode.tupled)
   }
 
-  def action[_: P]: P[DeclNode.FunctionNode] = {
+  private def action[_: P]: P[DeclNode.FunctionNode] = {
     P("action" ~/ identifier ~ parameters ~ block.?).map {
       case (name, parameters, body) => DeclNode.FunctionNode(name, parameters, TypeExprNode.UnitNode, body)
     }
   }
 
-  def parameters[_: P]: P[List[DeclNode.ParameterNode]] = {
+  private def parameters[_: P]: P[List[DeclNode.ParameterNode]] = {
     def parameter = P(identifier ~ ":" ~ typeExpression).map(DeclNode.ParameterNode.tupled)
     P("(" ~ parameter.rep(sep = ",") ~ ")").map(_.toList)
   }
@@ -52,19 +52,19 @@ object FragmentParser {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Type Declarations.
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  def typeDeclaration[_: P]: P[TypeDeclNode] = P(alias | label | `class`)
+  private def typeDeclaration[_: P]: P[TypeDeclNode] = P(alias | label | `class` | entity)
 
-  def alias[_: P]: P[TypeDeclNode.AliasNode] = P("type" ~/ identifier ~ "=" ~ typeExpression).map(TypeDeclNode.AliasNode.tupled)
-  def label[_: P]: P[TypeDeclNode.LabelNode] = P("label" ~/ identifier ~ `extends`).map(TypeDeclNode.LabelNode.tupled)
+  private def alias[_: P]: P[TypeDeclNode.AliasNode] = P("type" ~/ identifier ~ "=" ~ typeExpression).map(TypeDeclNode.AliasNode.tupled)
+  private def label[_: P]: P[TypeDeclNode.LabelNode] = P("label" ~/ identifier ~ `extends`).map(TypeDeclNode.LabelNode.tupled)
 
-  def `class`[_: P]: P[TypeDeclNode.ClassNode] = {
+  private def `class`[_: P]: P[TypeDeclNode.ClassNode] = {
     P("abstract".?.! ~ "class" ~/ identifier ~ `extends` ~ classBody(property)).map {
       case (abstractKeyword, name, supertypeName, (properties, constructors)) =>
         TypeDeclNode.ClassNode(name, supertypeName, abstractKeyword == "abstract", properties, constructors)
     }
   }
 
-  def entity[_: P]: P[TypeDeclNode.EntityNode] = {
+  private def entity[_: P]: P[TypeDeclNode.EntityNode] = {
     def ownedBy = P(("owned by" ~ typeExpression).?)
     P("abstract".?.! ~ "entity" ~/ identifier ~ ownedBy ~ `extends` ~ classBody(property | component)).map {
       case (abstractKeyword, name, ownedBy, supertypeName, (members, constructors)) =>
@@ -72,17 +72,17 @@ object FragmentParser {
     }
   }
 
-  def classBody[Member, _: P](member: => P[Member]): P[(List[Member], List[TypeDeclNode.ConstructorNode])] = {
+  private def classBody[Member, _: P](member: => P[Member]): P[(List[Member], List[TypeDeclNode.ConstructorNode])] = {
     def members = P(member.repX(sep = Space.terminators)).map(_.toList)
     def constructors = P(constructor.repX(sep = Space.terminators)).map(_.toList)
     P("{" ~ members ~ constructors ~ "}")
   }
-  def `extends`[_: P]: P[Option[String]] = P(("extends" ~ identifier).?)
-  def property[_: P]: P[TypeDeclNode.PropertyNode] = P("mut".?.! ~ identifier ~ ":" ~ typeExpression).map {
+  private def `extends`[_: P]: P[Option[String]] = P(("extends" ~ identifier).?)
+  private def property[_: P]: P[TypeDeclNode.PropertyNode] = P("mut".?.! ~ identifier ~ ":" ~ typeExpression).map {
     case (mutKeyword, name, tpe) => TypeDeclNode.PropertyNode(name, tpe, mutKeyword == "mut")
   }
-  def component[_: P]: P[TypeDeclNode.ComponentNode] = P("component" ~ identifier ~ ("overrides" ~ identifier).?).map(TypeDeclNode.ComponentNode.tupled)
-  def constructor[_: P]: P[TypeDeclNode.ConstructorNode] = {
+  private def component[_: P]: P[TypeDeclNode.ComponentNode] = P("component" ~ identifier ~ ("overrides" ~ identifier).?).map(TypeDeclNode.ComponentNode.tupled)
+  private def constructor[_: P]: P[TypeDeclNode.ConstructorNode] = {
     def constructorCall = P(("." ~ identifier).? ~ arguments).map(TypeDeclNode.ConstructorCallNode.tupled)
     def thisCall = P("this" ~ constructorCall)
     def superCall = P("super" ~ constructorCall)
