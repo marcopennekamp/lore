0a0b44925cc67925f193cf1cf5fbde4d8d1e5f23
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-12 15:24:43 +0200

    Add class/constructor test to FragmentParserSpec.

--- test/lore/test/parser/FragmentParserSpec.scala
+++ test/lore/test/parser/FragmentParserSpec.scala
@@ -13,6 +13,8 @@ class FragmentParserSpec extends BaseSpec with ParserSpecExtensions[DeclNode] {
 
   override def parser[_: P] = FragmentParser.topDeclaration
 
+  import TestNodes._
+
   "The function declaration parser" should "parse function declarations correctly" in {
     """
     |  function pow(x: Real, exp: Int): Real = {
@@ -68,5 +70,81 @@ class FragmentParserSpec extends BaseSpec with ParserSpecExtensions[DeclNode] {
         CallNode("damage", None, List(VariableNode("target"), VariableNode("power"))),
       ))),
     )
+  }
+
+  "The type declaration parser" should "parse class, property and constructor declarations correctly" in {
+    """
+    |  class Position {
+    |    x: Real
+    |    y: Real
+    |    z: Real
+    |
+    |    Position(x: Real, y: Real, z: Real) {
+    |      construct(x, y, z)
+    |    }
+    |
+    |    from2D(x: Real, y: Real) {
+    |      // We declare z like this to test whether the parser correctly separates the statement list
+    |      // and the continuation at the end.
+    |      const z = 0.0
+    |      this(x, y, z)
+    |    }
+    |
+    |    from1D(x: Real) {
+    |      const y = 0.0
+    |      this.from2D(x, y)
+    |    }
+    |  }
+    """.stripMargin --> ClassNode(
+      "Position", None, None, isAbstract = false,
+      List(
+        PropertyNode("x", tReal, isMutable = false),
+        PropertyNode("y", tReal, isMutable = false),
+        PropertyNode("z", tReal, isMutable = false),
+      ),
+      List(
+        ConstructorNode(
+          "Position",
+          List(
+            ParameterNode("x", tReal),
+            ParameterNode("y", tReal),
+            ParameterNode("z", tReal),
+          ),
+          BlockNode(List(
+            ConstructNode(List(vx, vy, vz), None),
+          )),
+        ),
+        ConstructorNode(
+          "from2D",
+          List(
+            ParameterNode("x", tReal),
+            ParameterNode("y", tReal),
+          ),
+          BlockNode(List(
+            VariableDeclarationNode("z", isMutable = false, None, RealLiteralNode(0.0)),
+            ConstructorCallNode(None, List(vx, vy, vz)),
+          )),
+        ),
+        ConstructorNode(
+          "from1D",
+          List(
+            ParameterNode("x", tReal),
+          ),
+          BlockNode(List(
+            VariableDeclarationNode("y", isMutable = false, None, RealLiteralNode(0.0)),
+            ConstructorCallNode(Some("from2D"), List(vx, vy)),
+          )),
+        )
+      ),
+    )
+    // TODO: Test abstract class, extends, owned by.
+  }
+
+  it should "fail on classes declaring component members" in {
+
+  }
+
+  it should "parse entity and component declarations correctly" in {
+
   }
 }
