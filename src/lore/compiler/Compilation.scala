79bdcacad582d2777621bc53f2ac6d3666e323a9
marcopennekamp <marco.pennekamp96@gmail.com>
2020-04-14 23:53:16 +0200

    Add CompilationPairExtension, which models simultaneous compilation.´

--- src/lore/compiler/Compilation.scala
+++ src/lore/compiler/Compilation.scala
@@ -80,4 +80,20 @@ object Compilation {
       if (errors.nonEmpty) Errors(errors, infos) else Result(results, infos)
     }
   }
+
+  /**
+    * Models simultaneous compilation resulting in a value of type (A, B).
+    */
+  implicit class CompilationPairExtension[A, B](cs: (Compilation[A], Compilation[B])) {
+    // TODO: If we want to expand this to heterogeneous lists of arbitrary length, we should consider using shapeless.
+    def combine: Compilation[(A, B)] = {
+      cs match {
+        case (Result(a, infosA), Result(b, infosB)) => Result((a, b), infosA ::: infosB)
+        case (ca, cb) =>
+          // As either ca or cb is guaranteed to be a failed compilation, combine will simply aggregate the errors
+          // and hence also produce an Errors object. This makes the type cast valid.
+          List(ca, cb).combine.asInstanceOf[Compilation[(A, B)]]
+      }
+    }
+  }
 }
