e8a386129b7a1542725d5659b97fc266e6c04a06
marcopennekamp <marco.pennekamp96@gmail.com>
2020-05-15 21:19:31 +0200

    Add ReturnConstraints spec.
    This is our first "error assertion" spec.

---
+++ test/lore/compiler/phases/verification/test/ReturnConstraintsSpec.scala
@@ -0,0 +1,15 @@
+package lore.compiler.phases.verification.test
+
+import lore.compiler.phases.verification.ReturnConstraints
+import lore.test.BaseSpec
+
+class ReturnConstraintsSpec extends BaseSpec {
+  "The compiler" should "compile return-constraints.lore with 'dead code' and 'impossible return' errors" in {
+    assertCompilationErrors("test/verification/return-constraints") { errors =>
+      assertErrorsMatchSignatures(errors, List(
+        ErrorSignature(classOf[ReturnConstraints.ImpossibleReturn], 5),
+        ErrorSignature(classOf[ReturnConstraints.DeadCode], 14),
+      ))
+    }
+  }
+}
--- test/lore/test/BaseSpec.scala
+++ test/lore/test/BaseSpec.scala
@@ -1,7 +1,8 @@
 package lore.test
 
 import lore.Lore
-import lore.compiler.Registry
+import lore.compiler.feedback.Error
+import lore.compiler.{Errors, Registry, Result}
 import lore.definitions.{FunctionDefinition, MultiFunctionDefinition}
 import lore.types.Type
 import org.scalatest._
@@ -15,6 +16,38 @@ abstract class BaseSpec extends AnyFlatSpec with Matchers with OptionValues with
   lazy val concatRegistry: Registry = prepareRegistry("concat")
 
   def prepareRegistry(exampleName: String): Registry = Lore.fromExample(exampleName).toOption.value
+
+  /**
+    * Assert that the given named source's compilation results in a list of errors, as required by the assertion.
+    * The list of errors is passed as sorted into the assertion function, in order of lines starting from line 1.
+    */
+  def assertCompilationErrors(name: String)(assert: List[Error] => Assertion): Assertion = {
+    Lore.fromExample(name) match {
+      case Result(_, _) => Assertions.fail(s"Compilation of $name should have failed with errors, but unexpectedly succeeded.")
+      case Errors(errors, _) => assert(errors.sortWith { case (e1, e2) => e1.position < e2.position })
+    }
+  }
+
+  /**
+    * A signature class that allows matching errors declaratively. You can extend this class to implement custom
+    * match functionality.
+    */
+  case class ErrorSignature(errorClass: Class[_], expectedLine: Int) {
+    def assertMatches(error: Error): Assertion = {
+      error.getClass shouldEqual errorClass
+      error.position.line shouldEqual expectedLine
+    }
+  }
+
+  /**
+    * Matches error lists that match the given list of error signatures. Errors have to match in order.
+    */
+  def assertErrorsMatchSignatures(errors: List[Error], signatures: List[ErrorSignature]): Assertion = {
+    errors should have length signatures.length
+    forAll(errors.zip(signatures)) { case (error, signature) =>
+      signature.assertMatches(error)
+    }
+  }
 
   implicit class MultiFunctionExtension(multiFunction: MultiFunctionDefinition) {
     def exactGet(inputType: Type): FunctionDefinition = multiFunction.exact(inputType).get
