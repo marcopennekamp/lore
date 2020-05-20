e2d49458b9ba41d17d0927947363a79c1b45a438
marcopennekamp <marco.pennekamp96@gmail.com>
2020-05-20 18:47:36 +0200

    Add LoreList.foreach and push.

--- runtime/src/lore/runtime/api/Values.scala
+++ runtime/src/lore/runtime/api/Values.scala
@@ -3,12 +3,23 @@ package lore.runtime.api
 import lore.types.{ListType, Type}
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import scala.scalajs.js.annotation.{JSExport, JSExportAll, JSExportTopLevel}
+
+@JSExportTopLevel("LoreList")
+@JSExportAll
+case class LoreList[A](array: js.Array[A], loreType: ListType) {
+  def forEach[R](f: js.Function1[A, R]): Unit = {
+    println("foreach")
+    for (e <- array) { f(e) }
+  }
+  def push(a: A): Unit = {
+    println(a)
+    array.push(a)
+  }
+}
 
 @JSExportTopLevel("Values")
 object Values {
-  case class LoreList[A](array: js.Array[A], loreType: ListType)
-
   @JSExport
   def list[A](array: js.Array[A], elementType: Type): LoreList[A] = LoreList(array, ListType(elementType))
 }
