07229f473138dd60abd28f94f0d0e2ac00472cd0
marcopennekamp <marco.pennekamp96@gmail.com>
2020-07-09 23:37:17 +0200

    Add standard timing for test runs.
    I'm trying not to optimize every single Lore program I write, but I like
    to at least have some overview of the general speed of different
    programs.

--- runtime/lore.js
+++ runtime/lore.js
@@ -1,3 +1,10 @@
 import { test } from '../lore-program.js';
 
+function toMus(before, after) {
+  return Math.round((after - before) * 1000);
+}
+
+const before = performance.now();
 test();
+const after = performance.now();
+console.log('Running test took ~' + toMus(before, after) + 'µs.');
