4d82d54c831ddfa951fe39d922e589efde5af86d
marcopennekamp <marco.pennekamp96@gmail.com>
2020-07-14 14:27:42 +0200

    Add output type hierarchy verification.

--- compiler/src/lore/compiler/phases/verification/MultiFunctionConstraints.scala
+++ compiler/src/lore/compiler/phases/verification/MultiFunctionConstraints.scala
@@ -8,21 +8,12 @@ import lore.compiler.functions.{FunctionDefinition, MultiFunctionDefinition}
 import lore.compiler.types.{Ards, Fit, Type}
 
 object MultiFunctionConstraints {
-  case class FunctionIllegallyAbstract(function: FunctionDefinition) extends Error(function) {
-    override def message: String = s"The function ${function.signature} is declared abstract even though it doesn't have an" +
-      s" abstract input type. Either implement the function or ensure the input type is abstract."
-  }
-
-  case class AbstractFunctionNotTotal(function: FunctionDefinition, missing: List[Type]) extends Error(function) {
-    override def message: String = s"The abstract function ${function.signature} is not fully implemented and thus doesn't" +
-      s" satisfy the totality constraint. Please implement functions for the following input types: ${missing.mkString(", ")}."
-  }
-
   /**
     * Verifies:
     *   1. No function body contains a continuation node.
     *   2. The input abstractness constraint.
     *   3. The totality constraint.
+    *   4. A child function's output type is a subtype of its parent function's output type.
     */
   def verify(mf: MultiFunctionDefinition)(implicit registry: Registry): Verification = {
     (
@@ -30,7 +21,7 @@ object MultiFunctionConstraints {
       verifyNoContinuation(mf),
       verifyInputAbstractness(mf),
       verifyTotalityConstraint(mf),
-      // TODO: Verify that return types are subtypes of return types of functions higher in the hierarchy.
+      verifyOutputTypes(mf),
     ).simultaneous.verification
   }
 
@@ -47,6 +38,11 @@ object MultiFunctionConstraints {
     }.simultaneous.verification
   }
 
+  case class FunctionIllegallyAbstract(function: FunctionDefinition) extends Error(function) {
+    override def message: String = s"The function ${function.signature} is declared abstract even though it doesn't have an" +
+      s" abstract input type. Either implement the function or ensure the input type is abstract."
+  }
+
   /**
     * Checks whether the given multi-function satisfies the input abstractness constraint.
     */
@@ -54,6 +50,11 @@ object MultiFunctionConstraints {
     Verification.fromErrors {
       mf.functions.filter(_.isAbstract).filterNot(_.signature.inputType.isAbstract).map(FunctionIllegallyAbstract)
     }
+  }
+
+  case class AbstractFunctionNotTotal(function: FunctionDefinition, missing: List[Type]) extends Error(function) {
+    override def message: String = s"The abstract function ${function.signature} is not fully implemented and thus doesn't" +
+      s" satisfy the totality constraint. Please implement functions for the following input types: ${missing.mkString(", ")}."
   }
 
   /**
@@ -94,5 +95,32 @@ object MultiFunctionConstraints {
         if (missing.nonEmpty) Some(AbstractFunctionNotTotal(function, missing)) else None
       }
     }
+  }
+
+  case class IncompatibleOutputTypes(child: FunctionDefinition, parent: FunctionDefinition) extends Error(child) {
+    override def message: String = s"The functions ${parent.signature} and ${child.signature} are in a hierarchical relationship, but the latter's" +
+      s" output type is not a subtype of the former's output type. Concretely, it should hold that ${child.signature.outputType} <:" +
+      s" ${parent.signature.outputType}, but this is not the case."
+  }
+
+  /**
+    * Verifies that the output types of the functions in the multi-function's hierarchy are compatible with each
+    * other. That is, a hierarchy child's output type must be a subtype of its hierarchy parent's output type.
+    */
+  def verifyOutputTypes(mf: MultiFunctionDefinition): Verification = {
+    def verifyHierarchyNode(node: mf.hierarchy.NodeT): Verification = {
+      val parent = node.value
+      val successors = node.diSuccessors.toList
+      successors.map { successor =>
+        val child = successor.value
+        println(s"$child <: $parent?")
+        val errors = if (!(child.signature.outputType <= parent.signature.outputType)) {
+          List(IncompatibleOutputTypes(child, parent))
+        } else Nil
+        Verification.fromErrors(errors).flatMap(_ => successors.map(verifyHierarchyNode).simultaneous.verification)
+      }.simultaneous.verification
+    }
+
+    mf.hierarchyRoots.map(verifyHierarchyNode).simultaneous.verification
   }
 }
