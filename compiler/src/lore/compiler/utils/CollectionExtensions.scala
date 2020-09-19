5110b622cacbfddec2e24304dc6a597d3a6472c3
marcopennekamp <marco.pennekamp96@gmail.com>
2020-09-19 21:36:12 +0200

    Fix ClassCastException in CollectionExtensions.
    Furthermore, the List implementation is now unused and thus removed.

--- compiler/src/lore/compiler/utils/CollectionExtensions.scala
+++ compiler/src/lore/compiler/utils/CollectionExtensions.scala
@@ -3,36 +3,27 @@ package lore.compiler.utils
 import scala.reflect.ClassTag
 
 object CollectionExtensions {
-  // We could implement the filter type extensions generically using SeqOps, but it would complicate the API to
-  // collection.filterType[A, Collection[A]], i.e. the programmer would have to respecify the That type because
-  // we need the type parameter A.
-
-  implicit class FilterTypeListExtension[A](list: List[A]) {
-    def filterType[T <: A](implicit tag: ClassTag[T]): List[T] = filterTypeImpl[A, T](list).asInstanceOf[List[T]]
-    def filterNotType[T <: A](implicit tag: ClassTag[T]): List[A] = filterNotTypeImpl[A, T](list).asInstanceOf[List[A]]
-  }
 
   implicit class FilterTypeVectorExtension[A](vector: Vector[A]) {
-    def filterType[T <: A](implicit tag: ClassTag[T]): Vector[T] = filterTypeImpl[A, T](vector).asInstanceOf[Vector[T]]
-    def filterNotType[T <: A](implicit tag: ClassTag[T]): Vector[A] = filterNotTypeImpl[A, T](vector).asInstanceOf[Vector[A]]
-  }
-
-  implicit class FilterTypeOptionExtension[A](option: Option[A]) {
-    def filterType[T <: A](implicit tag: ClassTag[T]): Option[T] = filterTypeImpl[A, T](option).asInstanceOf[Option[T]]
-    def filterNotType[T <: A](implicit tag: ClassTag[T]): Option[A] = filterNotTypeImpl[A, T](option).asInstanceOf[Option[A]]
-  }
-
-  private def filterTypeImpl[A, T <: A](collection: IterableOnce[A])(implicit tag: ClassTag[T]): IterableOnce[T] = {
-    collection.iterator.flatMap {
+    def filterType[T <: A](implicit tag: ClassTag[T]): Vector[T] = vector.flatMap {
       case value: T => Some(value)
       case _ => None
     }
-  }
-
-  private def filterNotTypeImpl[A, T <: A](collection: IterableOnce[A])(implicit tag: ClassTag[T]): IterableOnce[A] = {
-    collection.iterator.flatMap {
+    def filterNotType[T <: A](implicit tag: ClassTag[T]): Vector[A] = vector.flatMap {
       case _: T => None
       case value => Some(value)
     }
   }
+
+  implicit class FilterTypeOptionExtension[A](option: Option[A]) {
+    def filterType[T <: A](implicit tag: ClassTag[T]): Option[T] = option.flatMap {
+      case value: T => Some(value)
+      case _ => None
+    }
+    def filterNotType[T <: A](implicit tag: ClassTag[T]): Option[A] = option.flatMap {
+      case _: T => None
+      case value => Some(value)
+    }
+  }
+
 }
