09e162fc4d268665e944e223fee791efdf633af0
marcopennekamp <marco.pennekamp96@gmail.com>
2020-11-28 13:12:04 +0100

    Add an ownership test that SHOULD fail.

--- compiler/test/lore/compiler/phases/parsing/FragmentParserSpec.scala
+++ compiler/test/lore/compiler/phases/parsing/FragmentParserSpec.scala
@@ -11,54 +11,6 @@ class FragmentParserSpec extends BaseSpec with ParserSpecExtensions[Vector[DeclN
 
   import TestNodes._
 
-  it should "assign the correct indices" in {
-    "function add(a: Real, b: Real): Real = a + b".parsed.head match {
-      case fn: DeclNode.FunctionNode =>
-        fn.position.index shouldEqual 0
-        fn.parameters match {
-          case Seq(a, b) =>
-            a.position.index shouldEqual 13
-            b.position.index shouldEqual 22
-        }
-        fn.outputType.position.index shouldEqual 32
-        fn.body.value.position.index shouldEqual 39
-    }
-  }
-
-  it should "parse structs, traits, inheritance, ownership, and independence correctly" in {
-    """
-    |  independent trait L1
-    |  independent trait L2
-    |  struct P
-    |
-    |  trait O1 extends L1, L2
-    |  struct O2 implements O1
-    |
-    |  trait A owned by O1 & L1
-    |  struct B implements A, L2 owned by O2 & L1 {
-    |    mut property1: P
-    |    property2: P
-    |  }
-    """.stripMargin --> Vector(
-      Decl.Trait("L1", Vector.empty, Vector.empty, None, true),
-      Decl.Trait("L2", Vector.empty, Vector.empty, None, true),
-      Decl.Struct("P", Vector.empty, None, Vector.empty, false),
-      Decl.Trait("O1", Vector("L1", "L2"), Vector.empty, None, false),
-      Decl.Struct("O2", Vector("O1"), None, Vector.empty, false),
-      Decl.Trait("A", Vector.empty, Vector.empty, Some(Type.Intersection(Vector(Type.Identifier("O1"), Type.Identifier("L1")))), false),
-      Decl.Struct(
-        "B",
-        Vector("A", "L2"),
-        Some(Type.Intersection(Vector(Type.Identifier("O2"), Type.Identifier("L1")))),
-        Vector(
-          Decl.Property("property1", Type.Identifier("P"), true, None),
-          Decl.Property("property2", Type.Identifier("P"), false, None),
-        ),
-        false,
-      ),
-    )
-  }
-
   it should "parse entity and component declarations correctly" in {
     """
     |  struct C1 owned by E1
