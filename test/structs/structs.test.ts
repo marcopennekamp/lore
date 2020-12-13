bb359a8ac5a5e601875e146a4b37e015ce0ea523
marcopennekamp <marco.pennekamp96@gmail.com>
2020-12-13 17:03:35 +0100

    Add runtime-type-equality test.

--- test/structs/structs.test.ts
+++ test/structs/structs.test.ts
@@ -13,3 +13,8 @@ Deno.test('structs/position', async () => {
   const expected = [{ x: 1, y: 2, z: 3 }, { x: 0, y: 5, z: 0 }, { x: 20, y: 10, z: 0 }, { x: 7, y: 0, z: 0}]
   assertListForall(result, expected, (actual, expected) => assertStructHasValues(actual, 'Position', expected))
 })
+
+Deno.test('structs/runtime-type-equality', async () => {
+  const result: ListValue<boolean> = await LoreTest.run('structs/runtime-type-equality')
+  assertListEquals(result, [true, true, false, false, true, true, false, false])
+})
