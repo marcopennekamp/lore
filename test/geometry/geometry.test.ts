7e5e9dc8195252e0651758bf4daebe1ec55e2284
marcopennekamp <marco.pennekamp96@gmail.com>
2020-12-22 21:51:01 +0100

    Fix and refactor some functional tests.

--- test/geometry/geometry.test.ts
+++ test/geometry/geometry.test.ts
@@ -2,12 +2,14 @@ import { LoreTest } from '../base.ts'
 import { ListValue } from '../../runtime/src/lore/runtime/values/list.ts'
 import { assertListEquals } from '../assertions.ts'
 
-Deno.test('shapes/area', async () => {
-  const result: ListValue<number> = await LoreTest.run('shapes/area', 'shapes/shapes')
+const base = 'geometry'
+
+Deno.test(`${base}/area`, async () => {
+  const result: ListValue<number> = await LoreTest.run(`${base}/area`, `${base}/geometry`)
   assertListEquals(result, [250, 22.902210444671102, 1540])
 })
 
-Deno.test('shapes/comparisons', async () => {
-  const result: ListValue<string> = await LoreTest.run('shapes/comparisons', 'shapes/shapes')
+Deno.test(`${base}/comparison`, async () => {
+  const result: ListValue<string> = await LoreTest.run(`${base}/comparison`, `${base}/geometry`)
   assertListEquals(result, ['rect <= box', 'box <= rect', 'rect < circle', 'rect <= circle', 'box < circle', 'box <= circle'])
 })
--- test/structs/structs.test.ts
+++ test/structs/structs.test.ts
@@ -3,11 +3,6 @@ import { LoreTest } from '../base.ts'
 import { ObjectValue } from '../../runtime/src/lore/runtime/values/object.ts'
 import { assertListEquals, assertListForall, assertStructHasValues } from '../assertions.ts'
 
-Deno.test('structs/independent', async () => {
-  const result: ListValue<string> = await LoreTest.run('structs/independent')
-  assertListEquals(result, ['CA', 'CB'])
-})
-
 Deno.test('structs/position', async () => {
   const result: ListValue<ObjectValue> = await LoreTest.run('structs/position')
   const expected = [{ x: 1, y: 2, z: 3 }, { x: 0, y: 5, z: 0 }, { x: 20, y: 10, z: 0 }, { x: 7, y: 0, z: 0}]
