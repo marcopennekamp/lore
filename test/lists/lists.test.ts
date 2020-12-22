a2eb9618cb3f9479f17d9f889e6fa7dba8fddbea
marcopennekamp <marco.pennekamp96@gmail.com>
2020-12-22 23:38:54 +0100

    Turn the yield example into a functional test.

--- test/lists/lists.test.ts
+++ test/lists/lists.test.ts
@@ -1,6 +1,8 @@
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts'
 import { ListValue } from '../../runtime/src/lore/runtime/values/list.ts'
 import { LoreTest } from '../base.ts'
-import { assertListEquals } from '../assertions.ts'
+import { assertIsStruct, assertListEquals, assertListForall } from '../assertions.ts'
+import { ObjectValue } from '../../runtime/src/lore/runtime/values/object.ts'
 
 Deno.test('lists/concat', async () => {
   const result: ListValue<string> = await LoreTest.run('lists/concat')
@@ -13,4 +15,12 @@ Deno.test('lists/concat', async () => {
       'String/Int/Boolean list: [10, test, true, wow, 15]',
     ]
   )
+})
+
+Deno.test('lists/yield', async () => {
+  const result: ListValue<ObjectValue> = await LoreTest.run('lists/yield')
+  assertListForall(result, [2, 10, 20, 24], (actual, expected) => {
+    assertIsStruct(actual, 'Gold')
+    assertEquals((<any> actual).value, expected)
+  })
 })
