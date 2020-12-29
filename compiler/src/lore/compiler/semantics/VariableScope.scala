642ddd0f66188e577c7d12dc799a104c2e776824
marcopennekamp <marco.pennekamp96@gmail.com>
2020-12-29 23:04:46 +0100

    Move LocalVariable to its own file.
    It shouldn't have been part of VariableScope.scala, since that file should be limited to scope definitions.

--- compiler/src/lore/compiler/semantics/VariableScope.scala
+++ compiler/src/lore/compiler/semantics/VariableScope.scala
@@ -1,12 +1,6 @@
 package lore.compiler.semantics
 
-import lore.compiler.phases.transpilation.TranspiledName
 import lore.compiler.semantics.functions.FunctionSignature
-import lore.compiler.types.Type
-
-case class LocalVariable(name: String, tpe: Type, isMutable: Boolean) extends Scope.Entry {
-  lazy val transpiledName: TranspiledName = TranspiledName.localVariable(name)
-}
 
 /**
   * A scope that provides access to variables.
