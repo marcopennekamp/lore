ef7d83a3a558d0886b2d3c2d4b90b1ae71d80ae9
marcopennekamp <marco.pennekamp96@gmail.com>
2021-01-23 11:17:52 +0100

    Remove hasOwnProperty call from typeOf.
    Also note down a potential further optimization to inline typeOf calls if the compiler can sufficiently narrow down an argument's type.

--- runtime/src/lore/runtime/types/typeof.ts
+++ runtime/src/lore/runtime/types/typeof.ts
@@ -3,6 +3,10 @@ import { Type, Types } from './types.ts'
 
 /**
  * Calculates the Lore type of a value.
+ *
+ * TODO: If the compiler is sure that a given value is a struct, shape value, etc. with a lore$type attribute,
+ *       we should inline the lore$type access to remove the need of calling typeOf. This should bring sizable
+ *       performance gains.
  */
 export function typeOf(value: any): Type {
   switch (typeof value) {
@@ -20,7 +24,7 @@ export function typeOf(value: any): Type {
       // TODO: In the case of a Javascript object being given that does not have a type field, we should return some
       //       kind of "dynamic" type. Of course, this first requires us to define a similar notion within Lore
       //       itself.
-      if (value.hasOwnProperty('lore$type')) {
+      if ((<Value> value).lore$type) {
         return (<Value> value).lore$type
       }
       break
