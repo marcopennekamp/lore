5980059264f6248fffd7778d2ca7f0eb3d2d2810
marcopennekamp <marco.pennekamp96@gmail.com>
2021-02-12 22:03:04 +0100

    Add shape values to listify test.

--- test/shapes/shapes.test.ts
+++ test/shapes/shapes.test.ts
@@ -14,7 +14,10 @@ Deno.test('shapes/bunsnatcher', async () => {
 
 Deno.test('shapes/listify', async () => {
   const result: ListValue<ListValue<number>> = await LoreTest.run('shapes/listify')
-  const expected = [[1.2, 5], [0, 5.1, 4.8], [1.2, 5], [0, 5.1, 4.8]]
+  const expected = [
+    [1.2, 5], [0, 5.1, 4.8], [1.2, 5], [0, 5.1, 4.8],
+    [1.2, 5], [0, 5.1, 4.8], [1.2, 5], [0, 5.1, 4.8]
+  ]
   assertListForall(result, expected, (actual: ListValue<number>, expected: Array<number>) => {
     assertEquals(actual.array, expected)
   })
