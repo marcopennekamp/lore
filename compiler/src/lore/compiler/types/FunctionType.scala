1f64f781dc15ca9ee0f578304eb28a68d289d131
marcopennekamp <marco.pennekamp96@gmail.com>
2021-06-08 16:56:10 +0200

    Remove obsolete TODO.

--- compiler/src/lore/compiler/types/FunctionType.scala
+++ compiler/src/lore/compiler/types/FunctionType.scala
@@ -12,13 +12,6 @@ import scala.util.hashing.MurmurHash3
   *
   * The input type must always be a tuple type (or an inference variable) so that we can distinguish between functions
   * which take a single tuple argument vs. two distinct arguments. That is, `((A, B)) => C` vs. `(A, B) => C`.
-  *
-  * TODO: It's bothering me slightly that the input type has to be a tuple type. I'm tempted to lift this
-  *       "restriction", but there is one good argument for it: If FunctionType was Type => Type, the call syntax
-  *       wouldn't mirror this. For example, take a function `f: (Int, Int) => Int`. If we have a value
-  *       `v: (Int, Int)`, we should be able to call `f` with `v`. But how? `f(v)` would mean that an argument is
-  *       missing. `f v` would break even more assumptions. So it seems that for now the input type does indeed have
-  *       to be a tuple type.
   */
 case class FunctionType(input: Type, output: Type) extends Type {
   // This seems like the best way to (softly) enforce the need for tuple types here. A sum type would be great, but
