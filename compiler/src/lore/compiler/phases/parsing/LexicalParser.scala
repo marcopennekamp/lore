3496fd5e476e5b3d6ff008cda38fd10c8a600364
marcopennekamp <marco.pennekamp96@gmail.com>
2021-06-09 19:21:16 +0200

    Fix: The parser doesn't reject identifiers prefixed with a keyword anymore.
    - The parsing rule for keywords wasn't quite thought out: If an identifier STARTED with a keyword, such as `for` in `foreach`, the identifier was rejected.

--- compiler/src/lore/compiler/phases/parsing/LexicalParser.scala
+++ compiler/src/lore/compiler/phases/parsing/LexicalParser.scala
@@ -24,28 +24,24 @@ object LexicalParser {
   def fraction[_: P]: P[Unit]             = P("." ~ digit.rep(1))
 
   /**
-    * A keyword is a string that is used to signal some kind of specific language construct to the compiler. Not all
-    * keywords are automatically disallowed as identifiers: only critical keywords have that restriction.
-    */
-  def keyword[_: P]: P[Unit] = P(criticalKeyword | StringIn(
-    "action", "extends", "function", "implements", "mut", "struct", "trait", "type",
-  ))
-
-  /**
-    * A critical keyword is a keyword that, when used as an identifier in any place within the language, leads to
-    * ambiguity or confusion. For example, some keywords may not be consistently able to stand as variable names,
-    * such as `return` being possible in a declaration but not as a simple expression, as the parser will prefer to
-    * read `return` as a return top-level expression.
+    * A keyword is a string that, when used as an identifier in any place within the language, leads to ambiguity or
+    * confusion. For example, some keywords may not be consistently able to stand as variable names, such as `return`
+    * being possible in a declaration but not as a simple expression, as the parser will prefer to read `return` as a
+    * return top-level expression.
     *
     * Another example concerns the `dynamic` keyword: Declaring a function named "dynamic" would be possible, but
     * calling said function is impossible since `dynamic` gets parsed as a dynamic call.
+    *
+    * Some additional words may have special meaning to the parser, but aren't keywords, because the parser can resolve
+    * the ambiguity in all instances. These words are: action, extends, function, implements, mut, struct, trait, and
+    * type.
     */
-  def criticalKeyword[_: P]: P[Unit] = P(StringIn(
+  val keywords: Vector[String] = Vector(
     "dynamic", "else", "false", "for", "if", "let", "return", "super", "this", "true", "while",
-  ))
+  )
 
-  def identifier[_: P]: P[String] = P(!criticalKeyword ~ (letter | "_") ~ (letter | digit | "_").rep).!
-  def typeIdentifier[_: P]: P[String] = P(!criticalKeyword ~ (typeSpecialCharacter | letter | "_") ~ (typeSpecialCharacter | letter | digit | "_").rep).!
+  def identifier[_: P]: P[String] = P((letter | "_") ~ (letter | digit | "_").rep).!.filter(!keywords.contains(_))
+  def typeIdentifier[_: P]: P[String] = P((typeSpecialCharacter | letter | "_") ~ (typeSpecialCharacter | letter | digit | "_").rep).!.filter(!keywords.contains(_))
   def structIdentifier[_: P]: P[String] = P(identifier)
   def integer[_: P]: P[Long] = P(negatable[Long, Any](digitsNoTrailing.!.map(_.toLong)))
   def real[_: P]: P[Double] = P(negatable[Double, Any]((digitsNoTrailing ~ fraction).!.map(_.toDouble)))
