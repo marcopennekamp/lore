43189a69431c8ac1339ee2a1751948f3b681e20e
marcopennekamp <marco.pennekamp96@gmail.com>
2021-06-27 17:09:29 +0200

    Pyramid: Add symbolName function.

--- runtime/src/lore/runtime/symbols.ts
+++ runtime/src/lore/runtime/symbols.ts
@@ -1,4 +1,5 @@
 import { Kind } from './types/kinds.ts'
+import { typeOf } from './types/typeof.ts'
 import { Type } from './types/types.ts'
 import { stringHashWithSeed} from './utils/hash.ts'
 import { Value } from './values.ts'
@@ -19,5 +20,13 @@ export const Symbol = {
 
   value(type: SymbolType): SymbolValue {
     return { name: type.name, lore$type: type }
+  },
+
+  name(symbol: SymbolValue): string {
+    // See `pyramid/symbol.lore` on why this is necessary.
+    if (typeOf(symbol).kind !== Kind.Symbol) {
+      throw Error('The `Symbol.name` function must be invoked with a symbol value.')
+    }
+    return symbol.name
   },
 }
