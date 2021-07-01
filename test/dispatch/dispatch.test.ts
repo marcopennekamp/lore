c3b25b92afecd461ded6f7699d3507e1d64e2515
marcopennekamp <marco.pennekamp96@gmail.com>
2021-07-01 22:51:47 +0200

    Add dispatch/abstract-intersection test case.

--- test/dispatch/dispatch.test.ts
+++ test/dispatch/dispatch.test.ts
@@ -3,6 +3,11 @@ import { Kind } from '../../runtime/src/lore/runtime/types/kinds.ts'
 import { assertListEquals, assertSumTypeParts } from '../assertions.ts'
 import { LoreTest } from '../base.ts'
 
+Deno.test('dispatch/abstract-intersection', async () => {
+  const result: ListValue<string> = await LoreTest.run('dispatch/abstract-intersection.lore')
+  assertListEquals(result, ['A', 'C', 'B', 'C', 'C', 'B', 'C', 'B', 'C', 'B', 'X1|X2', 'X1|X2', 'X3'])
+})
+
 Deno.test('dispatch/abstract-partial-specialization', async () => {
   const result: ListValue<string> = await LoreTest.run('dispatch/abstract-partial-specialization.lore')
   assertListEquals(result, ['A1,B', 'A1,B', 'A2,B', 'A2,B'])
@@ -11,11 +16,6 @@ Deno.test('dispatch/abstract-partial-specialization', async () => {
 Deno.test('dispatch/abstract-sum', async () => {
   const result: ListValue<number> = await LoreTest.run('dispatch/abstract-sum.lore')
   assertListEquals(result, [3, 4, 4, 5, 5, 1, 2])
-})
-
-Deno.test('dispatch/abstract-sum-intersection', async () => {
-  const result: ListValue<string> = await LoreTest.run('dispatch/abstract-sum-intersection.lore')
-  assertListEquals(result, ['(A|B)&T', '(A|B)&T', 'C&T'])
 })
 
 Deno.test('dispatch/fixed', async () => {
