943cf949ca5b3e662bb44b160a5c0c7fd3599470
marcopennekamp <marco.pennekamp96@gmail.com>
2021-07-01 14:08:08 +0200

    Add test for "function illegally abstract" errors.

--- compiler/test/lore/compiler/phases/constraints/functions/MultiFunctionConstraintsSpec.scala
+++ compiler/test/lore/compiler/phases/constraints/functions/MultiFunctionConstraintsSpec.scala
@@ -6,6 +6,23 @@ import lore.compiler.test.BaseSpec
 class MultiFunctionConstraintsSpec extends BaseSpec {
   private val fragmentBase = "phases/constraints/functions"
 
+  "constraints/functions/illegally-abstract" should "be compiled with 'function illegally abstract' errors" in {
+    assertCompilationErrors(s"$fragmentBase/illegally-abstract.lore") { errors =>
+      assertErrorsMatchSignatures(errors, Vector(
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 7),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 9),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 11),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 13),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 15),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 17),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 19),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 21),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 23),
+        ErrorSignature(classOf[MultiFunctionConstraints.FunctionIllegallyAbstract], 25),
+      ))
+    }
+  }
+
   "constraints/functions/output-types" should "be compiled with 'incompatible output types' errors" in {
     assertCompilationErrors(s"$fragmentBase/incompatible-output-types.lore") { errors =>
       assertErrorsMatchSignatures(errors, Vector(
