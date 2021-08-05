e077bfdd2fba589550deafcca053e1ccbb37876c
marcopennekamp <marco.pennekamp96@gmail.com>
2021-08-05 19:10:40 +0200

    Add `option.lore` test.

--- test/schemas/schemas.test.ts
+++ test/schemas/schemas.test.ts
@@ -6,3 +6,8 @@ Deno.test('schemas/array', async () => {
   const result: ListValue<number> = await LoreTest.run('schemas/array.lore')
   assertListEquals(result, [2, 3, 4])
 })
+
+Deno.test('schemas/option', async () => {
+  const result: ListValue<string> = await LoreTest.run('schemas/option.lore')
+  assertListEquals(result, ['None', 'Some(Hello, world!)', 'Some([1, 2, 3])', 'Oh shit, it\'s a dragon!'])
+})
