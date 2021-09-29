02d6c63f486b6c7e77b09b9b8ecc3b4a34ee7ca2
marcopennekamp <marco.pennekamp96@gmail.com>
2021-09-29 22:29:00 +0200

    Pyramid: Add `Math.pow`.

--- runtime/src/lore/runtime/math.ts
+++ runtime/src/lore/runtime/math.ts
@@ -2,13 +2,20 @@ export const LoreMath = {
   floor(x: number): number {
     return Math.floor(x)
   },
+
   round(x: number): number {
     return Math.round(x)
   },
+
   ceil(x: number): number {
     return Math.ceil(x)
   },
+
   remainder(a: number, b: number): number {
-    return a % b;
+    return a % b
+  },
+
+  pow(base: number, exponent: number): number {
+    return Math.pow(base, exponent)
   },
 }
