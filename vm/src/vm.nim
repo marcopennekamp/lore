ba8c6c5367ae85bccf8ca9855816d8f8be383ad5
marcopennekamp <marco.pennekamp96@gmail.com>
2021-10-29 22:39:46 +0200

    VM: Run all examples once when in debug mode.

--- vm/src/vm.nim
+++ vm/src/vm.nim
@@ -1,15 +1,28 @@
 from examples/common import Example
 import evaluator
 import values
-from utils import when_release, benchmark
+from utils import when_release, when_debug, benchmark
+
+import examples/nine, examples/add_five, examples/fib
+
+proc run_example(example: Example) =
+  echo "Result: ", cast[IntValue](evaluator.evaluate(example.function, example.arguments))[]
 
 proc bench_example(example: Example) =
   benchmark(example.name, example.runs):
     discard evaluator.evaluate(example.function, example.arguments)
-  echo "Result: ", cast[IntValue](evaluator.evaluate(example.function, example.arguments))[]
+  run_example(example)
+
+let examples = [
+  nine.example,
+  add_five.example,
+  fib.example,
+]
 
 when_release:
-  import examples/nine, examples/add_five, examples/fib
-  bench_example(nine.example)
-  bench_example(add_five.example)
-  bench_example(fib.example)
+  for example in examples:
+    bench_example(example)
+
+when_debug:
+  for example in examples:
+    bench_example(example)
