8c101bc69d2be91c020273172fb779f58b950cf6
marcopennekamp <marco.pennekamp96@gmail.com>
2021-11-22 17:43:16 +0100

    VM: Rename "fixed seq" functions to "imseq".

--- vm/src/imseqs.nim
+++ vm/src/imseqs.nim
@@ -8,7 +8,7 @@ type
     len*: int
     elements*: UncheckedArray[T]
 
-proc new_fixed_seq*[T](source: open_array[T]): ImSeq[T] =
+proc new_immutable_seq*[T](source: open_array[T]): ImSeq[T] =
   let length = source.len
   # Note that `sizeof` of an unchecked array is 0, so we can use `sizeof(ImSeqObj[T])` to get the size of the preamble.
   var seq = cast[ImSeq[T]](alloc(sizeof(ImSeqObj[T]) + length * sizeof(T)))
@@ -16,11 +16,11 @@ proc new_fixed_seq*[T](source: open_array[T]): ImSeq[T] =
   copy_mem(addr seq.elements, unsafe_addr source, length * sizeof(T))
   seq
 
-let empty = new_fixed_seq[uint64]([])
+let empty = new_immutable_seq[uint64]([])
 
 ## Returns an empty immutable sequence for the given element type. This is backed by a single object reference so that
 ## reallocations do not have to occur.
-proc empty_fixed_seq*[T](): ImSeq[T] = cast[ImSeq[T]](empty)
+proc empty_immutable_seq*[T](): ImSeq[T] = cast[ImSeq[T]](empty)
 
 proc `[]`*[T](seq: ImSeq[T], index: int): T = seq.elements[index]
 
