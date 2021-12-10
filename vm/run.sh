51502f8fcc9efea6e27d846abbb2f90b4400e2b1
marcopennekamp <marco.pennekamp96@gmail.com>
2021-12-10 13:55:54 +0100

    VM: Add (commented out) debug run configuration.
    - GCC crashes when the Nim code is compiled with `debug` and LTO is turned on. So for debug runs, we have to disable LTO.

--- vm/run.sh
+++ vm/run.sh
@@ -5,4 +5,5 @@ export GC_INITIAL_HEAP_SIZE=10M
 #export GC_PRINT_STATS=true
 
 # --boundChecks:off --overflowChecks:off
+#nim compile --run -d:debug --gc:boehm --verbosity:0 --outdir:target -p:src src/${1:-vm.nim} ${@:2}
 nim compile --run -d:danger --passC:-flto --passL:-flto --gc:boehm --verbosity:0 --outdir:target -p:src src/${1:-vm.nim} ${@:2}
