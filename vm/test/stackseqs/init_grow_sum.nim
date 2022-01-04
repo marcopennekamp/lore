0938f6125a2655256cf1e4f765a43885dae21a55
marcopennekamp <marco.pennekamp96@gmail.com>
2022-01-04 21:23:18 +0100

    VM: Use blocks in testament tests.
    - Without the blocks, the variables declared in a test are actually global variables. This leads to name clashes across tests.

--- vm/test/stackseqs/init_grow_sum.nim
+++ vm/test/stackseqs/init_grow_sum.nim
@@ -6,35 +6,36 @@ import "../../src/stackseqs.nim"
 
 # Objective: StackSeqs should correctly initialize, grow, return the correct elements, and their iteration should lead
 # to the correct sum.
-var seq: StackSeq[8, int]
-for i in 1 .. 8:
-  seq.add(i)
+block:
+  var seq: StackSeq[8, int]
+  for i in 1 .. 8:
+    seq.add(i)
 
-assert seq.heap_data == nil
-assert seq.heap_cap == 0
+  assert seq.heap_data == nil
+  assert seq.heap_cap == 0
 
-for i in 0 ..< 8:
-  assert seq[i] == i + 1
+  for i in 0 ..< 8:
+    assert seq[i] == i + 1
 
-# This should force the StackSeq to grow into the heap.
-seq.add(42)
+  # This should force the StackSeq to grow into the heap.
+  seq.add(42)
 
-assert seq.heap_data != nil
-assert seq.heap_cap == 8
-assert seq[8] == 42
+  assert seq.heap_data != nil
+  assert seq.heap_cap == 8
+  assert seq[8] == 42
 
-# And the cap should be raised again when we add another 12 elements.
-for i in 0 ..< 12:
-  seq.add(1)
+  # And the cap should be raised again when we add another 12 elements.
+  for i in 0 ..< 12:
+    seq.add(1)
 
-assert seq.len == 21
-assert seq.heap_cap == 16
-for i in 9 .. 20:
-  assert seq[i] == 1
+  assert seq.len == 21
+  assert seq.heap_cap == 16
+  for i in 9 .. 20:
+    assert seq[i] == 1
 
-# Finally, we can test iteration by summing all values.
-var sum = 0
-for n in seq:
-  sum += n
+  # Finally, we can test iteration by summing all values.
+  var sum = 0
+  for n in seq:
+    sum += n
 
-assert sum == 90
+  assert sum == 90
