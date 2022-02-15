efb424d5248891e8f5b594bb9f5ea34d3b0b18be
marcopennekamp <marco.pennekamp96@gmail.com>
2022-02-15 19:19:46 +0100

    Compiler (fix): Registers must start at ID 0.

--- compiler/src/lore/compiler/assembly/RegisterProvider.scala
+++ compiler/src/lore/compiler/assembly/RegisterProvider.scala
@@ -5,10 +5,12 @@ import lore.compiler.poem.Poem
 import java.util.concurrent.atomic.AtomicInteger
 
 class RegisterProvider {
-  private val counter: AtomicInteger = new AtomicInteger()
+  private val counter: AtomicInteger = new AtomicInteger(0)
 
   /**
     * Provides a fresh, unique register ID.
+    *
+    * The first ID is guaranteed to be 0.
     */
-  def fresh(): Poem.Register = Poem.Register(counter.incrementAndGet())
+  def fresh(): Poem.Register = Poem.Register(counter.getAndIncrement())
 }
