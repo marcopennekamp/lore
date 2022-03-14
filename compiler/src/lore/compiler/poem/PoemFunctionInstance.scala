4d84695c7f65e6cd6be699000b1ae49ff833b485
marcopennekamp <marco.pennekamp96@gmail.com>
2022-03-14 17:04:18 +0100

    Compiler: Improve PoemInstruction stringification.

--- compiler/src/lore/compiler/poem/PoemFunctionInstance.scala
+++ compiler/src/lore/compiler/poem/PoemFunctionInstance.scala
@@ -3,5 +3,8 @@ package lore.compiler.poem
 import lore.compiler.semantics.NamePath
 
 case class PoemFunctionInstance(name: NamePath, typeArguments: Vector[PoemType]) {
-  override def toString: String = s"$name[${typeArguments.mkString(", ")}]"
+  override def toString: String = {
+    if (typeArguments.isEmpty) name.toString
+    else s"$name[${typeArguments.mkString(", ")}]"
+  }
 }
