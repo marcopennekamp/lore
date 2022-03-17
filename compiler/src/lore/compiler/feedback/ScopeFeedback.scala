e2cd08b08ebd8b283f389ba43656193bfb6b47a6
marcopennekamp <marco.pennekamp96@gmail.com>
2022-03-17 18:09:06 +0100

    Compiler: Enclose names in backticks in ScopeFeedback.

--- compiler/src/lore/compiler/feedback/ScopeFeedback.scala
+++ compiler/src/lore/compiler/feedback/ScopeFeedback.scala
@@ -4,18 +4,18 @@ import lore.compiler.core.Position
 
 object ScopeFeedback {
   case class AlreadyDeclared(label: String, name: String, override val position: Position) extends Feedback.Error(position) {
-    override def message = s"The $label $name has already been declared in the current scope."
+    override def message = s"The $label `$name` has already been declared in the current scope."
   }
 
   case class UnknownEntry(label: String, name: String, override val position: Position) extends Feedback.Error(position) {
-    override def message = s"The $label $name does not exist in the current scope."
+    override def message = s"The $label `$name` does not exist in the current scope."
   }
 
   case class ModuleNotFound(name: String, override val position: Position) extends Feedback.Error(position) {
-    override def message = s"The binding $name is not a module or does not exist in the current scope."
+    override def message = s"The binding `$name` is not a module or does not exist in the current scope."
   }
 
   case class ModuleExpected(name: String, override val position: Position) extends Feedback.Error(position) {
-    override def message = s"The binding $name must be a module."
+    override def message = s"The binding `$name` must be a module."
   }
 }
