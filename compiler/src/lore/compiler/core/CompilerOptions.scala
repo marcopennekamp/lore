3e0999adeae3e1ab1182bbf5e1dd7c0b6eb57705
marcopennekamp <marco.pennekamp96@gmail.com>
2022-04-21 19:35:28 +0200

    Compiler: Remove obsolete `enableRuntimeLogging` compiler CLI option.

--- compiler/src/lore/compiler/core/CompilerOptions.scala
+++ compiler/src/lore/compiler/core/CompilerOptions.scala
@@ -2,13 +2,6 @@ package lore.compiler.core
 
 case class CompilerOptions(
   /**
-    * Run-time logging generates various logging statements in the target Javascript code, which can greatly affect performance.
-    *
-    * TODO (assembly): Remove this option, as it has no place with the VM as a runtime.
-    */
-  enableRuntimeLogging: Boolean = false,
-
-  /**
     * Report errors and warnings with stack traces.
     */
   showFeedbackStackTraces: Boolean = false,
