2d46c06bfa0e2c92263f4657b46919df6c916955
marcopennekamp <marco.pennekamp96@gmail.com>
2022-04-22 18:28:28 +0200

    Compiler: Fix optimization of noop jumps.
    - See the added comment inside `NoopOptimizer.optimize` for an explanation of what went wrong and how we fixed it.

--- compiler/src/lore/compiler/assembly/optimization/NoopOptimizer.scala
+++ compiler/src/lore/compiler/assembly/optimization/NoopOptimizer.scala
@@ -4,6 +4,11 @@ import lore.compiler.poem.PoemInstruction
 
 object NoopOptimizer {
 
+  // TODO: More optimization (as seen in lore.List.repeat):
+  //          IntAdd reg4 <- reg2 reg4
+  //          Assign reg2 <- reg4
+  //       This could be contracted into a single instruction: `IntAdd reg2 <- reg2 reg4`.
+
   /**
     * Optimizes:
     *   - `Assign(source, target)`: If `source` and `target` are the same registers, the `Assign` instruction can be
@@ -16,21 +21,27 @@ object NoopOptimizer {
     * instructions to refer to label locations, not absolute locations.
     */
   def optimize(instructions: Vector[PoemInstruction]): Vector[PoemInstruction] = {
-    if (instructions.length < 2) {
-      return instructions
-    }
-
-    val optimizedInstructions = instructions.sliding(2).flatMap { case Vector(instruction, nextInstruction) =>
+    // We need to iterate through the instructions in reverse order so that no-op jumps can be optimized away in one
+    // go. For example:
+    //
+    //    Jump <label>
+    //    Assign reg0 <- reg0
+    //    <label>: Assign reg1 <- reg2
+    //
+    // The `Jump` should be removed as a no-op, but only when `Assign` is removed first. Going through the instructions
+    // in reverse order will do the trick.
+    //
+    // If `instructions` is empty or only contains a single instruction, the fold will simply return the instruction
+    // list unchanged. The last instruction will not be optimized, but since it must be a `Return` instruction, it
+    // wouldn't be a no-op in any case.
+    instructions.init.foldRight(instructions.lastOption.toVector) { case (instruction, result) =>
       // If `instruction` is deemed to be a no-op, it is removed and its labels are given to `nextInstruction`.
+      val nextInstruction = result.head
       if (isNoop(instruction, nextInstruction)) {
         nextInstruction.preserveLabelsOf(instruction)
-        Vector.empty
-      } else Vector(instruction)
-    }.toVector
-
-    // The last instruction will never be on the left-hand side of the `sliding` window, so we have to add it manually.
-    // This instruction will be a `Return` instruction, so it wouldn't be a no-op in any case.
-    optimizedInstructions :+ instructions.last
+        result
+      } else instruction +: result
+    }
   }
 
   private def isNoop(instruction: PoemInstruction, nextInstruction: PoemInstruction): Boolean = instruction match {
