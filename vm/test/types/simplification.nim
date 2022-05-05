473473fb885acbe202c3f4c038ccceae9558803b
marcopennekamp <marco.pennekamp96@gmail.com>
2022-05-05 19:10:07 +0200

    VM: Fix `simplification.nim` test.

--- vm/test/types/simplification.nim
+++ vm/test/types/simplification.nim
@@ -5,40 +5,38 @@ discard """
 import "../../src/imseqs.nim"
 import "../../src/types.nim"
 
-# TODO (assembly): Fix this test.
-
 # Objective: Ensure that type simplification works correctly for non-trivial types.
 block:
-  let primitives1 = new_immutable_seq([real_type, int_type, sum([int_type, string_type]), sum([boolean_type])])
-  let primitives2 = new_immutable_seq([int_type, real_type, intersection([int_type, string_type]), sum([boolean_type])])
-  let primitives3 = new_immutable_seq([int_type, real_type, intersection([int_type, string_type]), intersection([boolean_type])])
-  let primitives4 = new_immutable_seq([int_type, real_type, sum([int_type, string_type]), intersection([boolean_type])])
+  let primitives1 = new_immutable_seq([real_type, int_type, new_sum_type([int_type, string_type]), new_sum_type([boolean_type])])
+  let primitives2 = new_immutable_seq([int_type, real_type, new_intersection_type([int_type, string_type]), new_sum_type([boolean_type])])
+  let primitives3 = new_immutable_seq([int_type, real_type, new_intersection_type([int_type, string_type]), new_intersection_type([boolean_type])])
+  let primitives4 = new_immutable_seq([int_type, real_type, new_sum_type([int_type, string_type]), new_intersection_type([boolean_type])])
 
   assert $sum_simplified(primitives1) == "(Real | Int | String | Boolean)"
   assert $sum_simplified(primitives2) == "(Int | Real | Boolean)"
   assert $intersection_simplified(primitives3) == "(Int & Real & String & Boolean)"
   assert $intersection_simplified(primitives4) == "(Int & Real & Boolean)"
 
-  let tuples0 = new_immutable_seq([tpl_as_type([]), tpl([])])
-  let tuples1 = new_immutable_seq([tpl_as_type([int_type]), tpl([intersection_as_type([int_type, real_type])])])
+  let tuples0 = new_immutable_seq([tuple_as_type([]), new_tuple_type([])])
+  let tuples1 = new_immutable_seq([tuple_as_type([int_type]), new_tuple_type([intersection_as_type([int_type, real_type])])])
   let tuples234 = new_immutable_seq([
-    tpl_as_type([int_type, int_type]),
-    tpl([boolean_type, int_type, string_type, int_type]),
-    tpl([int_type, int_type, real_type]),
-    tpl([real_type, int_type]),
-    tpl([real_type, int_type, real_type]),
-    tpl([boolean_type, int_type, real_type, int_type]),
-    tpl([int_type, string_type, real_type]),
+    tuple_as_type([int_type, int_type]),
+    new_tuple_type([boolean_type, int_type, string_type, int_type]),
+    new_tuple_type([int_type, int_type, real_type]),
+    new_tuple_type([real_type, int_type]),
+    new_tuple_type([real_type, int_type, real_type]),
+    new_tuple_type([boolean_type, int_type, real_type, int_type]),
+    new_tuple_type([int_type, string_type, real_type]),
   ])
   let tuples2569 = new_immutable_seq([
-    tpl_as_type([int_type, int_type, int_type, int_type, int_type, real_type]),
-    tpl([int_type, real_type]),
-    tpl([int_type, int_type, real_type, int_type, int_type]),
-    tpl([int_type, int_type, int_type, int_type, real_type, int_type, int_type, int_type, int_type]),
-    tpl([int_type, real_type, int_type, int_type, int_type, int_type]),
-    tpl([real_type, real_type]),
-    tpl([int_type, int_type, int_type, int_type, int_type, int_type, int_type, int_type, int_type]),
-    tpl([int_type, int_type, int_type, real_type, int_type, int_type]),
+    tuple_as_type([int_type, int_type, int_type, int_type, int_type, real_type]),
+    new_tuple_type([int_type, real_type]),
+    new_tuple_type([int_type, int_type, real_type, int_type, int_type]),
+    new_tuple_type([int_type, int_type, int_type, int_type, real_type, int_type, int_type, int_type, int_type]),
+    new_tuple_type([int_type, real_type, int_type, int_type, int_type, int_type]),
+    new_tuple_type([real_type, real_type]),
+    new_tuple_type([int_type, int_type, int_type, int_type, int_type, int_type, int_type, int_type, int_type]),
+    new_tuple_type([int_type, int_type, int_type, real_type, int_type, int_type]),
   ])
 
   assert $sum_simplified(tuples0) == "()"
@@ -50,18 +48,18 @@ block:
   assert $intersection_simplified(tuples2569) == "(((Int & Real), Real) & (Int, Int, Real, Int, Int) & (Int, (Int & Real), Int, (Int & Real), Int, (Real & Int)) & (Int, Int, Int, Int, (Real & Int), Int, Int, Int, Int))"
 
   let functions1 = new_immutable_seq([
-    function_as_type(tpl([sum_as_type([int_type, real_type])]), sum([string_type, boolean_type])),
-    function(tpl([real_type]), boolean_type),
-    function(tpl([int_type]), string_type),
+    function_as_type(new_tuple_type([sum_as_type([int_type, real_type])]), new_sum_type([string_type, boolean_type])),
+    new_function_type(new_tuple_type([real_type]), boolean_type),
+    new_function_type(new_tuple_type([int_type]), string_type),
   ])
 
   assert $sum_simplified(functions1) == "(((Real & Int)) => (String | Boolean))"
   assert $intersection_simplified(functions1) == "(((Int | Real)) => (Boolean & String))"
 
   let lists1 = new_immutable_seq([
-    list_as_type(tpl([int_type, real_type, real_type])),
-    list_as_type(tpl([boolean_type])),
-    list_as_type(tpl([int_type, int_type, string_type])),
+    list_as_type(new_tuple_type([int_type, real_type, real_type])),
+    list_as_type(new_tuple_type([boolean_type])),
+    list_as_type(new_tuple_type([int_type, int_type, string_type])),
   ])
 
   assert $sum_simplified(lists1) == "[((Boolean) | (Int, (Real | Int), (Real | String)))]"
