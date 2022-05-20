f5924bfc8d8741eb035dda84adb7a9358244473e
marcopennekamp <marco.pennekamp96@gmail.com>
2022-05-20 18:15:43 +0200

    Tests: Refactor and speccify `calculator`.

--- test/calculator/calculator.test.ts
+++
@@ -1,8 +0,0 @@
-import { ListValue } from '../../runtime/src/lore/runtime/lists.ts'
-import { assertListEquals } from '../assertions.ts'
-import { LoreTest } from '../base.ts'
-
-Deno.test('calculator', async () => {
-  const result: ListValue<number> = await LoreTest.run('calculator')
-  assertListEquals(result, [0, 3, 7, 0.5, -35])
-})
