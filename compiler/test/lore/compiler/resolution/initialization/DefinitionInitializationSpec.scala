52c0f1b328f7612f6b0cd8d83a135a74aeeec5a0
marcopennekamp <marco.pennekamp96@gmail.com>
2022-07-14 18:01:06 +0200

    Compiler: Add test for fallback-initialization of cyclic types and struct aliases.

---
+++ compiler/test/lore/compiler/resolution/initialization/DefinitionInitializationSpec.scala
@@ -0,0 +1,23 @@
+package lore.compiler.resolution.initialization
+
+import lore.compiler.feedback.{AliasFeedback, SchemaFeedback, StructFeedback}
+import lore.compiler.test.BaseSpec
+
+class DefinitionInitializationSpec extends BaseSpec {
+
+  private val fragmentBase = "resolution/initialization"
+
+  s"$fragmentBase/fallbacks" should "compile without crashing and report errors" in {
+    // This ensures that fallback-initialization of cyclic type definitions and struct aliases without an underlying
+    // struct type is carried out correctly. Especially, a compiler crash due to uninitialized binding definitions must
+    // be avoided at all costs. The clarity of the complete mass of reported errors is of secondary importance.
+    assertCompilationErrorSignatures(s"$fragmentBase/fallbacks.lore", exitCompilationEarly = false)(
+      (classOf[SchemaFeedback.InheritanceCycle], 5),
+      (classOf[SchemaFeedback.InheritanceCycle], 8),
+      (classOf[AliasFeedback.StructExpected], 15),
+      (classOf[AliasFeedback.ObjectExpected], 19),
+      (classOf[StructFeedback.Object.MemberNotFound], 30),
+    )
+  }
+
+}
