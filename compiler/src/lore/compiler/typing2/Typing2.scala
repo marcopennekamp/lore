00867c20269c6a12727e2ae0b55d582fe7e3f0ed
marcopennekamp <marco.pennekamp96@gmail.com>
2022-08-25 21:07:13 +0200

    Compiler: Add loggers to Typing2.

--- compiler/src/lore/compiler/typing2/Typing2.scala
+++ compiler/src/lore/compiler/typing2/Typing2.scala
@@ -1,11 +1,17 @@
 package lore.compiler.typing2
 
+import com.typesafe.scalalogging.Logger
 import lore.compiler.semantics.expressions.Expression
-import lore.compiler.typing.Typing.logger
+import lore.compiler.utils.IndentationLogger
 
 // TODO (multi-import): Check that important attempts and results are traced (especially call/function value typing).
+//                      Refactor and overhaul all current traces.
 
 object Typing2 {
+
+  val indentationLogger: IndentationLogger = IndentationLogger("lore.compiler.typing")
+  val logger: Logger = Logger(indentationLogger)
+  val loggerBlank: Logger = Logger("lore.compiler.typing.blank")
 
   def traceExpressionType(expression: Expression, label: String, additional: String = ""): Unit = {
     logger.whenTraceEnabled {
