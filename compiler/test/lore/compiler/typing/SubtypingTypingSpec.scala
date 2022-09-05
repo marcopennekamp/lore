9dcc0745f22ce3d399e02156d0a80112b1e1657b
marcopennekamp <marco.pennekamp96@gmail.com>
2022-09-05 13:31:12 +0200

    Compiler: Fix `subtyping_errors` test.

--- compiler/test/lore/compiler/typing/SubtypingTypingSpec.scala
+++ compiler/test/lore/compiler/typing/SubtypingTypingSpec.scala
@@ -9,8 +9,7 @@ class SubtypingTypingSpec extends BaseSpec {
   s"$fragmentBase/subtyping_errors" should "be compiled with typing errors" in {
     assertCompilationErrorMessages(s"$fragmentBase/subtyping_errors.lore")(
       ("This expression has the illegal type `A`. We expected the following type (or a subtype thereof): B.", 13),
-      ("A construction of `Ox` cannot result in expected type `Fox`, because `Fox` cannot be specialized to `Ox`." +
-        " Most likely, `Ox` is not a subtype of `Fox`.", 20),
+      ("This expression has the illegal type `Ox`. We expected the following type (or a subtype thereof): Fox.", 20),
       ("This expression has the illegal type `Unit`. We expected the following type (or a subtype thereof): Real.", 25),
       ("This expression has the illegal type `Real`. We expected the following type (or a subtype thereof): String.", 31),
       ("This expression has the illegal type `Animal`. We expected the following type (or a subtype thereof): Fox.", 40),
