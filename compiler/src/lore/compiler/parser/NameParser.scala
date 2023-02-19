3a37d9d466c14ec830a88205144e6a14e3de870f
Marco Pennekamp <marco.pennekamp96@gmail.com>
2023-02-19 23:34:58 +0100

    [compiler] Update `NameParser` to return results

--- compiler/src/lore/compiler/parser/NameParser.scala
+++ compiler/src/lore/compiler/parser/NameParser.scala
@@ -3,23 +3,25 @@ package lore.compiler.parser
 import lore.compiler.core.Position
 import lore.compiler.syntax.Node.{NameNode, NamePathNode}
 import lore.compiler.syntax.{TkDot, TkIdentifier, TkPlus}
-import scalaz.Scalaz.ToOptionIdOps
 
 import scala.collection.mutable
 
+/**
+  * Callees are expected to report an error if a name parser fails.
+  */
 trait NameParser { _: Parser =>
   def name(): Result[NameNode] = consume[TkIdentifier].map(token => NameNode(token.value, token.position))
 
   /**
     * A type name might be composed of several connected [[TkIdentifier]] and [[TkPlus]] tokens.
     */
-  def typeName(): Option[NameNode] = {
+  def typeName(): Result[NameNode] = {
     val connectedTokens = consumeConnectedTokens {
       case _: TkIdentifier => true
       case _: TkPlus => true
       case _ => false
     }
-    if (connectedTokens.isEmpty) return None
+    if (connectedTokens.isEmpty) return Failure
 
     val stringBuilder = new mutable.StringBuilder()
     connectedTokens.foreach {
@@ -30,16 +32,16 @@ trait NameParser { _: Parser =>
 
     val startIndex = connectedTokens.head.startIndex
     val endIndex = connectedTokens.last.endIndex
-    NameNode(stringBuilder.toString(), Position(fragment, startIndex, endIndex)).some
+    NameNode(stringBuilder.toString(), Position(fragment, startIndex, endIndex)).success
   }
 
-  def namePath(): Option[NamePathNode] = genericNamePath(name())
+  def namePath(): Result[NamePathNode] = genericNamePath(name())
 
-  def typeNamePath(): Option[NamePathNode] = genericNamePath(typeName())
+  def typeNamePath(): Result[NamePathNode] = genericNamePath(typeName())
 
-  private def genericNamePath(name: => Option[NameNode]): Option[NamePathNode] = {
+  private def genericNamePath(name: => Result[NameNode]): Result[NamePathNode] = {
     val names = collectSepBacktrack(consumeIf[TkDot]) { name }
-    if (names.isEmpty) return None
-    Some(NamePathNode(names))
+    if (names.isEmpty) return Failure
+    NamePathNode(names).success
   }
 }
